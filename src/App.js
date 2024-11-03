20c144cafc7c9561c3b6567c9528dee221124943
NhaLinh <trannhalinh262@gmail.com>
2024-11-03 12:45:15 +0700

    a

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // src/App.js
 import React from 'react';
 import SpotifyLogin from './components/SpotifyLogin';
+import Header from './Header/header'; // Import Header
 import './App.css'; // Đảm bảo import file CSS
 
 function App() {
   return (
     <div>
+      <Header /> {/* Hiển thị Header với logo */}
       <div className="background-image" id="backgroundImage" />
       <SpotifyLogin />
     </div>
--- src/components/TrackItem.jsx
+++ src/components/TrackItem.jsx
@@ -1,6 +1,9 @@
+// src/components/TrackItem.jsx
 import React, { useState, useRef } from 'react';
 import Img from './img';
-import Button from './button';
+import { IconButton } from '@mui/material';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import PauseIcon from '@mui/icons-material/Pause';
 import './TrackItem.css';
 
 const TrackItem = ({ track, onPlay }) => {
@@ -20,17 +23,19 @@ const TrackItem = ({ track, onPlay }) => {
   };
 
   return (
-    <div className="track-item"> {/* Thêm class này */}
+    <div className="track-item">
       <Img src={track.track.album.images[0].url} alt={`${track.track.name} album cover`} />
       <div className="track-info">
-        <p style={{ fontWeight: 'bold', margin: '0' }}>{track.track.name.length > 40 ? `${track.track.name.substring(0, 40)}...` : track.track.name}</p>
+        <p style={{ fontWeight: 'bold', margin: '0' }}>{track.track.name}</p>
         <p style={{ margin: '0' }}>{track.track.artists.map(artist => artist.name).join(', ')}</p>
+
+        <IconButton onClick={handlePlayPause} className="play-pause-button">
+          {isPlaying ? <PauseIcon /> : <PlayArrowIcon />}
+        </IconButton>
       </div>
-      <Button isPlaying={isPlaying} onClick={handlePlayPause} />
       <audio ref={audioRef} src={track.track.preview_url} />
     </div>
-);
-
+  );
 };
 
 export default TrackItem;
